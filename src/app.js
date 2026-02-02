d4cf138625a93c2ffa3f69e5148a057e7200d509
Tinashe Malvin Zuze <23p31a05r2@acet.ac.in>
2026-02-02 11:01:32 +0530

    It is about to go down

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const cors=require("cors")
+
+const app=express();
+app.use(cors())
+const port=3000;
+
+app.get("/",(req,res)=>{
+    res.send(`Welcome to the server `);
+});
+
+app.get("/name/:name",(req,res)=>{
+    const name=req.params.name;
+    res.send(`My name is ${name} `);
+});
+
+
+app.listen(port,()=>{
+    console.log(`The server is running on http://localhost:${port}`);
+
+})
+//console.log("A pleasant day to you all, My name is Bla Bla Bla");
